e0e8fa1afb8ea3e4d6e028dd8dc7673742f012b6
Serhii Zlobin <serhii@booqable.com>
2023-03-30 11:28:48 +0300

    menu styling, close menu and header modals
    1. header and menu styling
    2. closing mobile menu on resize to desktop
    3. closing mobile menu and search/account modals on click header icons

--- assets/header.js
+++ assets/header.js
@@ -1,21 +1,39 @@
-const selects = {
+const selector = {
   body: "body",
   previewBar: ".preview-bar__container",
-  announcementBar: ".announcement-bar"
+  announcementBar: ".announcement-bar",
+  menuBottom: ".header-menu-bottom",
+  menu: ".menu",
+  menuItem: ".menu__item",
+  menuLink: ".menu__link",
+  input: "input[type=checkbox]",
+  menuTrigger: "#mobile-menu-trigger",
+  searchTrigger: "#floating-search",
+  accountTrigger: "#account-opener"
+};
+
+const classes = {
+  headerSticky: "header--sticky",
+  menuItem: "menu__item",
+  menuLink: "menu__link",
+  menuOpener: "menu__dropdown-opener"
 };
 
 const modifiers = {
-  scrolled: "scroll-down"
+  scrolled: "scroll-down",
+  overflow: "overflow-hidden"
 };
 
 const props = {
-  transform: '--header-transform',
   height: '--header-height',
   barHeight: '--announcement-bar-height',
-  previewHeight: '--preview-bar-height'
+  linkHeight: '--menu-link-height',
+  previewHeight: '--preview-bar-height',
+  transform: '--header-transform'
 }
 
 const minHeight = 180;
+const mobilePoint = 1100;
 
 class Header {
   constructor(container) {
@@ -32,33 +50,44 @@ class Header {
   }
 
   initElements() {
-    this.body = document.querySelector(selects.body);
-    this.previewBar = document.querySelector(selects.previewBar);
-    this.announcementBar = document.querySelector(selects.announcementBar);
+    this.html = document.documentElement;
+    this.body = document.querySelector(selector.body);
+    this.previewBar = document.querySelector(selector.previewBar);
+    this.announcementBar = this.container.querySelector(selector.announcementBar);
+    this.isSticky = this.container.classList.contains(classes.headerSticky);
+    this.isMenuBottom = this.container.querySelector(selector.menuBottom);
+    this.menu = this.container.querySelector(selector.menu);
+    this.menuTrigger = this.container.querySelector(selector.menuTrigger);
+    this.menuItem = this.container.querySelector(selector.menuItem);
+    this.menuItems = [...this.container.querySelectorAll(selector.menuItem)];
+    this.openers = [...this.menu.querySelectorAll(selector.input)];
+    this.searchTrigger = this.container.querySelector(selector.searchTrigger);
+    this.accountTrigger = this.container.querySelector(selector.accountTrigger);
     this.lastScroll = 0;
-
-    // setTimeout(() => {
-    this.getHeaderHeight();
-    // this.getHeaderHeight();
-    // }, 100); // header height fix on load on iPhone
   }
 
   initEvents() {
+    this.getHeaderHeight();
+    this.setMenuBottomPosition();
+
+    document.addEventListener("click", this.headerIconsClick.bind(this));
+    document.addEventListener("mouseover", this.closeHeaderModals.bind(this));
     window.addEventListener("scroll", this.setPropsOnScroll.bind(this));
     window.addEventListener("resize", this.getHeaderHeight.bind(this));
+    window.addEventListener("resize", this.setMenuBottomPosition.bind(this));
+    window.addEventListener("resize", this.closeMobileMenuResize.bind(this));
   }
 
-  // getting height of header
+  // getting height of header and set css variables
   getHeaderHeight() {
     let headerHeight = this.container.getBoundingClientRect().height,
-      announcementBarHeight = 0,
-      previewBarHeight = 0;
+        announcementBarHeight = 0,
+        previewBarHeight = 0;
 
     if (this.announcementBar) {
       announcementBarHeight = this.announcementBar.getBoundingClientRect().height;
-      // headerHeight += announcementBarHeight;
 
-      document.documentElement.style.setProperty(
+      this.html.style.setProperty(
         `${props.barHeight}`,
         `${announcementBarHeight}px`
       );
@@ -66,62 +95,168 @@ class Header {
 
     if (this.previewBar) {
       previewBarHeight = this.previewBar.getBoundingClientRect().height;
-      // headerHeight += previewBarHeight;
+      headerHeight += previewBarHeight;
 
-      document.documentElement.style.setProperty(
+      this.html.style.setProperty(
         `${props.previewHeight}`,
         `${previewBarHeight}px`
       );
     }
 
-    document.documentElement.style.setProperty(
+    this.html.style.setProperty(
       `${props.height}`,
       `${Math.floor(headerHeight) - 1}px`
     );
-
   }
 
   // setting properties when scroll page
   setPropsOnScroll() {
-    if (!this.announcementBar) {
-      return false;
-    }
+    if (!this.announcementBar) return false;
 
+    this.isScrolled = this.body.classList.contains(modifiers.scrolled);
     this.currentScroll = window.scrollY;
 
     if (this.currentScroll <= minHeight) {
       this.body.classList.remove(modifiers.scrolled);
-      document.documentElement.style.setProperty(
+      this.html.style.setProperty(
         `${props.transform}`,
         '0'
       );
       return;
     }
 
-    if (
-      this.currentScroll > this.lastScroll &&
-      !this.body.classList.contains(modifiers.scrolled)
-    ) {
-      // down
-      this.body.classList.add(modifiers.scrolled);
-      document.documentElement.style.setProperty(
-        `${props.transform}`,
-        `-${this.announcementBar.offsetHeight}px`
-      );
-    } else if (
-      this.currentScroll < this.lastScroll - 10 &&
-      this.body.classList.contains(modifiers.scrolled)
-    ) {
-      // up
-      this.body.classList.remove(modifiers.scrolled);
-      document.documentElement.style.setProperty(
-        `${props.transform}`,
-        '0'
-      );
-    }
+    this.currentScroll > this.lastScroll && !this.isScrolled
+      ? // down
+        (this.body.classList.add(modifiers.scrolled),
+        this.html.style.setProperty(
+          `${props.transform}`,
+          `-${this.announcementBar.offsetHeight}px`
+        ))
+      : this.currentScroll < this.lastScroll - 10 && this.isScrolled
+        ? // up
+          (this.body.classList.remove(modifiers.scrolled),
+          this.html.style.setProperty(
+            `${props.transform}`,
+            '0'
+          ))
+      : null
 
     this.lastScroll = this.currentScroll;
 
+  }
+
+  // add css variable when desktop menu is in bottom mode for menu positioning
+  setMenuBottomPosition() {
+    if (!this.isMenuBottom) return false;
+
+    let itemHeight = this.menuItem.getBoundingClientRect().height;
+
+    this.html.style.setProperty(
+      `${props.linkHeight}`,
+      `${itemHeight}px`
+    );
+  }
+
+  // adding overflow:hidden when menu opened while header is not sticky on mobile
+  // closing modals of search and account, and mobile menu on click on header icons
+  // closing all dropdowns when menu closed on mobile
+  headerIconsClick(e) {
+    let target = e.target,
+        targetTrigger = target.previousElementSibling;
+
+    switch (targetTrigger) {
+      case this.menuTrigger:
+        this.accountTrigger.checked = false;
+        this.searchTrigger.checked = false;
+
+        this.menuTrigger.checked
+          ? this.closeMobileMenu()
+          : this.addOverflow()
+
+        break;
+
+      case this.searchTrigger:
+        this.menuTrigger.checked = false;
+        this.accountTrigger.checked = false;
+        this.closeMobileDropMenu();
+        this.removeOverflow();
+
+        break;
+
+      case this.accountTrigger:
+        this.menuTrigger.checked = false;
+        this.searchTrigger.checked = false;
+        this.closeMobileDropMenu();
+        this.removeOverflow();
+
+        break;
+    }
+  }
+
+  closeMobileMenu() {
+      this.removeOverflow();
+      this.closeMobileDropMenu();
+  }
+
+  closeMobileMenuResize() {
+    window.innerWidth >= mobilePoint
+      ? (this.closeMobileMenu(),
+        this.menuTrigger.checked = false)
+      : null
+  }
+
+  closeMobileDropMenu() {
+    this.openers.forEach(opener => opener.checked = false)
+  }
+
+  closeHeaderModals(e) {
+    if (!this.menuItems.length) return false;
+
+    let target = e.target,
+        targetTrigger,
+        items;
+
+    switch (target.classList[0]) {
+      case classes.menuItem:
+        if (!target.firstElementChild.classList.contains(classes.menuLink)) {
+          items = [...this.container.querySelectorAll(selector.menuItem)];
+          targetTrigger = target.firstElementChild;
+        }
+
+        break;
+
+      case classes.menuLink:
+        if (target.parentElement.firstElementChild.classList.contains(classes.menuLink)) {
+          return false;
+        }
+
+        break;
+
+      case classes.menuOpener:
+        items = [...this.container.querySelectorAll(selector.menuLink)];
+        targetTrigger = target.parentElement.firstElementChild;
+
+        break;
+    }
+
+    if (!items.length) return false;
+
+    items.forEach(item => {
+      if (target === item) {
+        if (targetTrigger) {
+          this.accountTrigger.checked = false;
+          this.searchTrigger.checked = false;
+        }
+      }
+    })
+  }
+
+  addOverflow() {
+    !this.isSticky ? this.html.classList.add(modifiers.overflow) : null
+  }
+
+  removeOverflow() {
+    !this.isSticky ? this.html.classList.remove(modifiers.overflow) : null
   }
 }
 
