0155f18243de390b88da53e42c3f808f040bb6c8
Serhii Zlobin <serhii@booqable.com>
2023-04-03 20:35:42 +0300

    Finished collections section
    Collections section was recoded.
    Also, added image placeholder in settings. Added js convertion hex to rgb for primary color. Added styles on scrollbar

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,75 @@
-const modificators = {
-  loaded: "loaded",
-  resizeClass: "resize-active"
+const val = {
+  load: "loaded",
+  res: "resize-active"
 };
+
+const vars = {
+  hex: "--color-primary-foreground",
+  rgb: "--color-primary-foreground-rgb"
+}
 
 class Main {
   constructor(container) {
-    this.container = container;
+    this.cnt = container;
   }
 
   init() {
-    if (!this.container) {
+    if (!this.cnt) {
       return false;
     }
 
-    this.timer = undefined;
+    this.elements();
+    this.events();
+  }
 
-    this.setLoadedClass();
-    window.addEventListener("resize", this.setResizeClass.bind(this));
+  elements() {
+    this.doc = document.documentElement;
+    this.tmr = undefined;
+  }
+
+  events() {
+    this.setClass();
+    this.toRgb();
+    window.addEventListener("resize", this.setResize.bind(this));
   }
 
   // adding class while resizing window
-  setResizeClass() {
-    this.container.classList.add(modificators.resizeClass);
-    clearTimeout(this.timer);
+  setResize() {
+    this.cnt.classList.add(val.res);
+    clearTimeout(this.tmr);
 
-    this.timer = setTimeout(() => {
-      this.container.classList.remove(modificators.resizeClass);
+    this.tmr = setTimeout(() => {
+      this.cnt.classList.remove(val.res);
     }, 500);
   }
 
   // adding class after loading content
-  setLoadedClass() {
-    this.container.classList.add(modificators.loaded);
+  setClass() {
+    this.cnt.classList.add(val.load);
+  }
+
+  // convert the color of primary css var to RGB
+  toRgb() {
+    const hexToRgb = hex =>
+      hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+            .substring(1).match(/.{2}/g)
+            .map(x => parseInt(x, 16))
+
+    const hex = getComputedStyle(this.doc)
+                  .getPropertyValue(vars.hex).trim();
+
+    const rgb = `${hexToRgb(hex)}`
+
+    this.doc.style.setProperty(
+                    `${vars.rgb}`,
+                    `${rgb}`
+                  );
   }
 }
 
 const main = new Main(document.querySelector('body'));
 
-document.addEventListener("readystatechange", (event) => {
-  if (event.target.readyState === "complete") main.init();
+document.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === "complete") main.init();
 });
