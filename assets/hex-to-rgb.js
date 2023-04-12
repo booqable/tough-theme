c0e1cc6fe206b3fd1c0f841b0fdf0c73cefdb54e
Serhii Zlobin <serhii@booqable.com>
2023-04-12 17:17:31 +0300

    image with text section

---
+++ assets/hex-to-rgb.js
@@ -0,0 +1,43 @@
+class ToRgb {
+  constructor(section, options) {
+    this.section = section;
+    this.options = options;
+  }
+
+  init() {
+    if (!this.section) return false;
+
+    this.toRgb();
+  }
+
+  // convert the color of css var to RGB
+  toRgb() {
+    const hexToRgb = hex =>
+      hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+              ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+            .substring(1).match(/.{2}/g)
+            .map(x => parseInt(x, 16))
+
+    const doc = document.documentElement;
+
+    let hex = getComputedStyle(this.section)
+                .getPropertyValue(this.options.hex).trim();
+
+    console.log(this.options.hex);
+    console.log(this.section);
+    console.log(hex);
+
+    // if (hex) {
+    //   hex = getComputedStyle(doc)
+    //           .getPropertyValue(this.options.hex).trim();
+    // }
+
+    const rgb = `${hexToRgb(hex)}`;
+
+    this.section.style.setProperty(`${this.options.rgb}`, `${rgb}`);
+  }
+}
+
+window.ToRgb = ToRgb
+
+
---
+++ assets/image-with-text.js
@@ -0,0 +1,27 @@
+class ImageWithText {
+  constructor(section) {
+    this.section = section;
+    this.options = {
+      hex: "--color-primary-foreground",
+      rgb: "--color-primary-foreground-rgb"
+    }
+  }
+
+  init() {
+    if (!this.section) return false;
+
+    const init = new window.ToRgb(this.section, this.options);
+    init.init();
+  }
+}
+
+((selector = ".image-text__wrapper") => {
+  const sections = [...document.querySelectorAll(selector)];
+
+  if (!sections.length) return false;
+
+  sections.forEach(section => {
+    const imageWithText = new ImageWithText(section);
+    imageWithText.init();
+  });
+})();
