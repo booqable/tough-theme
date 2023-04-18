c210fd7224dbd449c39357c58bf0e27084a5d45c
Serhii Zlobin <serhii@booqable.com>
2023-04-18 16:50:27 +0300

    added autorotate for carousel, cleanup code

--- assets/hero.js
+++ assets/hero.js
@@ -1,21 +1,85 @@
 class Hero {
   constructor(section) {
     this.section = section;
+
+    this.selector = {
+      image: ".hero__images",
+      button: ".carousel__trigger",
+      timer: ".carousel__timer"
+    }
+
+    this.classes = {
+      next: "next"
+    }
+
+    this.id = {
+      slide: "slide"
+    }
+
     this.options = {
       hex: "--color-primary-foreground",
       rgb: "--color-primary-foreground-rgb"
     }
+
+    this.current = 1
   }
 
   init() {
     if (!this.section) return false;
 
-    const init = new window.ToRgb(this.section, this.options);
-    init.init();
+    this.elements();
+    this.events();
+  }
+
+  elements() {
+    this.images = [...this.section.querySelectorAll(this.selector.image)];
+    this.buttons = [...this.section.querySelectorAll(this.selector.button)];
+    this.timer = this.section.querySelector(this.selector.timer);
+  }
+
+  events() {
+    this.toRgb();
+    this.event();
+    this.autoRotate();
+  }
+
+  toRgb() {
+    if (!this.images.length) return false;
+
+    this.images.forEach(image => {
+      const init = new window.ToRgb(image, this.options);
+      init.init();
+    })
+  }
+
+  autoRotate() {
+    const timer = this.timer.value * 1000;
+
+    if (timer === 0 ) return false;
+
+    const interval = setInterval(() => {
+      this.event()
+    }, timer)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }
+
+  event() {
+    if (!this.buttons.length) return false;
+
+    const toggler = document.querySelector(`#${this.id.slide}-${this.current}`)
+
+    toggler.click();
+
+    this.current === this.buttons.length
+      ? this.current = 1
+      : this.current += 1
   }
 }
 
-((selector = ".hero__images") => {
+function initHero(selector = ".hero") {
   const sections = [...document.querySelectorAll(selector)];
 
   if (!sections.length) return false;
@@ -24,10 +88,8 @@ class Hero {
     const hero = new Hero(section);
     hero.init();
   });
-})();
+};
 
-// document.addEventListener("readystatechange", (e) => {
-//   if (e.target.readyState === "complete") {
-//     initSection()
-//   }
-// });
+document.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === "complete") initHero()
+});
