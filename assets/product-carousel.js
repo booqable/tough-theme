1d7b47f07548ac331f9e300fee67b54fd2b700b6
Serhii Zlobin <serhii@booqable.com>
2023-05-01 11:32:48 +0300

    new carousel

--- assets/product-carousel.js
+++ assets/product-carousel.js
@@ -1,34 +1,124 @@
 class Carousel {
-  constructor(section) {
-    this.section = section;
+  constructor(block) {
+    this.block = block;
 
     this.selector = {
-
+      pagItem: ".carousel__bullet",
+      box: ".products__carousel-wrapper",
+      item: ".products__carousel-item"
     }
 
-    this.modificators = {
-      loaded: "loaded",
-      resizeClass: "resize-active"
+    this.classes = {
+      pag: "carousel__bullet",
+      nav: "carousel__button",
+      prev: "prev",
+      next: "next"
+    }
+
+    this.modifiers = {
+      active: "active"
+    }
+
+    this.data = {
+      index: "data-index"
     };
   }
 
   init() {
-    if (!this.section) {
-      return false;
-    }
+    if (!this.block) return false;
 
     this.elements();
     this.events();
   }
 
   elements() {
+    this.box = this.block.querySelector(this.selector.box);
+    this.item = this.block.querySelector(this.selector.item);
+    this.arr = [...this.block.querySelectorAll(this.selector.pagItem)];
   }
 
   events() {
-    // window.addEventListener("resize", this.setResizeClass.bind(this));
+    document.addEventListener("click", this.navigation.bind(this));
+    document.addEventListener("click", this.pagination.bind(this));
   }
 
+  navigation(e) {
+    const target = e.target,
+          isEl = target.classList.contains(this.classes.pag),
+          isNav = target.classList.contains(this.classes.nav);
 
+    if (!isNav && !isEl) return false;
+
+    const list = target.classList,
+          width = this.item.getBoundingClientRect().width,
+          scroll = this.box.scrollWidth,
+          client = this.box.clientWidth,
+          index = parseInt(target.getAttribute(this.data.index));
+
+    let val,
+        i,
+        left = this.box.scrollLeft;
+
+    switch (true) {
+      case list.contains(this.classes.prev):
+        val = left === 0 ? scroll : left - width;
+
+        left === 0 ? i = this.arr.length : i = Math.ceil(left / width)
+
+        this.pagination(e, i)
+
+        break;
+      case list.contains(this.classes.next):
+        val = left >= scroll - client ? left = 0 : left + width;
+
+        if(left + width >= scroll - client) {
+          i = this.arr.length
+        } else {
+          console.log(left);
+          console.log(width);
+          console.log(Math.floor(left / width + 1));
+          // if (left < width) {
+          //   i = 1
+          // } else {
+            i = Math.floor(left / width + 1)
+          // }
+          // console.log(i);
+        }
+
+        this.pagination(e, i)
+
+        break;
+      case list.contains(this.classes.pag):
+        val = width * (index - 1);
+
+        break;
+    }
+
+    this.scrollTo(val);
+  }
+
+  pagination(e, i) {
+    const target = e.target,
+          isEl = target.classList.contains(this.classes.pag);
+
+    if (!isEl && i === undefined) return false;
+
+    this.arr.forEach(el => {
+      const val = parseInt(el.getAttribute(this.data.index));
+
+      el.classList.remove(this.modifiers.active);
+
+      if (isEl) target.classList.add(this.modifiers.active);
+      if (val === i) el.classList.add(this.modifiers.active);
+    })
+  }
+
+  scrollTo(val) {
+    this.box.scrollTo({
+      left: val,
+      behavior: "smooth",
+    });
+  }
 }
 
 function initCarousel(selector = ".products__carousel") {
