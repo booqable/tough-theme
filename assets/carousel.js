bc2319ea9a538de3cd45472e42275a5400ff09d9
Serhii Zlobin <serhii@booqable.com>
2023-05-04 18:11:53 +0300

    added fade effect, counter change, pause on hover

--- assets/carousel.js
+++ assets/carousel.js
@@ -3,6 +3,7 @@ class Carousel {
     this.block = block;
 
     this.selector = {
+      button: ".carousel__button",
       bullet: ".carousel__bullet",
       wrapper: ".carousel__wrapper",
       item: ".carousel__item",
@@ -11,8 +12,11 @@ class Carousel {
     }
 
     this.classes = {
-      bullet: "carousel__bullet",
+      show: "show",
+      fade: "carousel__fade-effect",
+      pause: "carousel__pause",
       button: "carousel__button",
+      bullet: "carousel__bullet",
       prev: "prev",
       next: "next",
       init: "initialized"
@@ -23,7 +27,6 @@ class Carousel {
     }
 
     this.data = {
-      id: "id",
       index: "data-index"
     };
   }
@@ -39,6 +42,7 @@ class Carousel {
     this.wrap = this.block.querySelector(this.selector.wrapper);
     this.item = this.block.querySelector(this.selector.item);
     this.items = [...this.block.querySelectorAll(this.selector.item)];
+    this.buttons = [...this.block.querySelectorAll(this.selector.button)];
     this.bullets = [...this.block.querySelectorAll(this.selector.bullet)];
     this.count = this.block.querySelector(this.selector.count);
     this.timer = this.block.querySelector(this.selector.timer).value * 1000;
@@ -46,18 +50,37 @@ class Carousel {
   }
 
   events(e) {
-    this.sliderInit();
-    // this.autoRotate(e, this.timer);
+    this.carouselInit();
+    this.autoRotate(e, this.timer);
     this.pauseRotate();
 
-    document.addEventListener("click", this.navigation.bind(this));
-    document.addEventListener("click", this.pagination.bind(this));
+    this.listener(this.bullets, 'click', this.pagination)
+    this.listener(this.bullets, 'click', this.navigation)
+    this.listener(this.buttons, 'click', this.navigation)
   }
 
-  sliderInit() {
+  listener(arr, event, call) {
+    arr.forEach(el => {
+      el.addEventListener(`${event}`, call.bind(this));
+    })
+  }
+
+  carouselInit() {
     if (this.items.length < 2) return false;
 
     this.block.classList.add(this.classes.init);
+  }
+
+  activeSlide(val) {
+    const isEl = this.block.classList.contains(this.classes.fade);
+
+    if (!isEl) return false;
+    if (typeof val === 'undefined') return false;
+
+    this.items.forEach((el, i) => {
+      el.classList.replace('show', 'hide');
+      if (i + 1 === val) el.classList.replace('hide', 'show');
+    })
   }
 
   // autorotate slides of carousel
@@ -75,11 +98,14 @@ class Carousel {
 
   // pause autorotate slides on hover and touch devices
   pauseRotate() {
+    const isEl = this.block.classList.contains(this.classes.pause);
+
+    if (!isEl) return false;
     if (!this.items.length) return false;
 
     const event = e => {
-      // if(e.type === "mouseenter" || e.type === "touchstart") clearInterval(this.interval)
-      // if(e.type === "mouseleave" || e.type === "touchend") this.autoRotate(e, this.timer)
+      if(e.type === "mouseenter" || e.type === "touchstart") clearInterval(this.interval)
+      if(e.type === "mouseleave" || e.type === "touchend") this.autoRotate(e, this.timer)
     }
 
     this.block.addEventListener('mouseenter', event);
@@ -93,7 +119,7 @@ class Carousel {
     const target = e?.target,
           isEl = target?.classList.contains(this.classes.bullet);
 
-    if (!isEl && typeof i === 'undefined') return false;
+          if (!isEl && typeof i === 'undefined') return false;
 
     this.bullets.forEach(el => {
       const val = parseInt(el.getAttribute(this.data.index));
@@ -102,14 +128,19 @@ class Carousel {
 
       if (isEl) target.classList.add(this.modifiers.active);
       if (val === i) el.classList.add(this.modifiers.active);
+      if (typeof i === 'undefined') i = parseInt(target.getAttribute(this.data.index));
     })
+
+    this.counter(e, i)
+    this.activeSlide(i)
   }
 
   // slide the carousel left/right and change the index of the active bullet of the pagination
   navigation(e, t) {
     const target = e?.target,
           isBullet = target?.classList.contains(this.classes.bullet),
-          isNav = target?.classList.contains(this.classes.button);
+          isNav = target?.classList.contains(this.classes.button),
+          isFade = this.block.classList.contains(this.classes.fade);
 
     if (!isNav && !isBullet && t === 0 && typeof t === 'undefined') return false;
 
@@ -117,7 +148,8 @@ class Carousel {
           width = this.item.getBoundingClientRect().width,
           scroll = this.wrap.scrollWidth,
           client = this.wrap.clientWidth,
-          index = parseInt(target?.getAttribute(this.data.index));
+          index = parseInt(target?.getAttribute(this.data.index)),
+          children = [...this.wrap.children];
 
     let val,
         i,
@@ -125,41 +157,75 @@ class Carousel {
 
     switch (true) {
       case list?.contains(this.classes.prev):
-        left === 0 ? i = Math.ceil((scroll - client) / width + 1) : i = Math.ceil(left / width)
-        this.pagination(e, i)
+        if (!isFade) {
+          left === 0
+            ? i = Math.ceil((scroll - client) / width +1)
+            : i = Math.ceil(left / width),
+          this.pagination(e, i),
+          val = left === 0 ? scroll : left - width
 
-        val = left === 0 ? scroll : left - width;
+        } else {
+          children.forEach((el, ind) => {
+            if (el.classList.contains(this.classes.show)) {
+              ind === 0 ? i = this.items.length : i = ind;
+            }
+          })
 
-        // this.counter(10)
+          this.pagination(e, i);
+          this.activeSlide(i)
+        }
 
         break;
       case list?.contains(this.classes.next):
-        left >= scroll - client ? i = 1 : i = parseInt(left / width + 2)
-        this.pagination(e, i)
+        if (!isFade) {
+          left >= scroll - client ? i = 1 : i = parseInt(left / width + 2)
+          this.pagination(e, i)
 
-        val = left >= scroll - client ? left = 0 : left + width;
+          val = left >= scroll - client ? left = 0 : left + width;
+        } else {
+          children.forEach((el, ind) => {
+            if (el.classList.contains(this.classes.show)) {
+              ind + 1 === this.items.length ? i = 1 : i = ind + 2;
+            }
+          })
+
+          this.pagination(e, i);
+          this.activeSlide(i)
+        }
 
         break;
       case list?.contains(this.classes.bullet):
-        val = width * (index - 1);
+        if (!isFade) val = width * (index - 1);
 
         break;
       case t !== 0 && typeof t !== 'undefined':
         if (this.items.length < 2) return false;
 
-        left >= scroll - client ? i = 1 : i = parseInt(left / width + 2)
-        this.pagination(e, i)
+        if (!isFade) {
+          left >= scroll - client ? i = 1 : i = parseInt(left / width + 2)
+          this.pagination(e, i)
 
-        val = left + client >= scroll ? left = 0 : left += width
+          val = left + client >= scroll ? left = 0 : left += width
+        } else {
+
+          children.forEach((el, ind) => {
+            if (el.classList.contains(this.classes.show)) {
+              ind + 1 === this.items.length ? i = 1 : i = ind + 2;
+            }
+          })
+
+          this.pagination(e, i);
+          this.activeSlide(i)
+        }
 
         break;
     }
 
-    this.scrollTo(val);
+    if (!isFade) this.scrollTo(val);
   }
 
   // change index of counter of slides
-  counter(i) {
+  counter(e, i) {
     if (!this.count) return false;
 
     if (i === 0 || typeof i === 'undefined') return false;
@@ -184,9 +250,7 @@ function initCarousel(el = ".carousel") {
 
   if (!arr.length) return false;
 
-  arr.forEach((val, i) => {
-    val.setAttribute('id', `carousel-${i}`);
-
+  arr.forEach(val => {
     const carousel = new Carousel(val);
     carousel.init();
   });
