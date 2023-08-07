1338644c1da4969dfaac2fc8ad6953e7ffa90ef0
Serhii Zlobin <serhii@booqable.com>
2023-08-07 17:42:28 +0300

    Added map marker

--- assets/maps.js
+++ assets/maps.js
@@ -4,6 +4,7 @@ class Map {
 
     this.selector = {
       map: ".map",
+      marker: ".map-marker__image",
       link: ".tabs__link"
     };
 
@@ -61,9 +62,31 @@ class Map {
       };
 
       const renderMap = (obj) => {
+        const marker = document.querySelector(this.selector.marker),
+              lon = obj[0]?.lon,
+              lat = obj[0]?.lat;
+
         const layers = [
           new ol.layer.Tile({
             source: new ol.source.OSM()
+          }),
+          new ol.layer.Vector({
+            source: new ol.source.Vector({
+              features: [
+                new ol.Feature({
+                  geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat]))
+                })
+              ]
+            }),
+            style: new ol.style.Style({
+              image: new ol.style.Icon({
+                anchor: [0.5, 60],
+                anchorXUnits: 'fraction',
+                anchorYUnits: 'pixels',
+                imgSize: [40, 55],
+                img: marker
+              })
+            })
           })
         ];
 
@@ -76,7 +99,7 @@ class Map {
           layers: layers,
           interactions: interaction,
           view: new ol.View({
-            center: ol.proj.fromLonLat([obj[0]?.lon, obj[0]?.lat]),
+            center: ol.proj.fromLonLat([lon, lat]),
             zoom: this.zoom
           })
         });
@@ -91,9 +114,7 @@ class Map {
       }
 
       getData().then(data => {
-        data.length
-          ? (renderMap(data), this.getUrl(data))
-          : message()
+        data.length ? (renderMap(data), this.getUrl(data)) : message()
       });
     });
   }
@@ -101,19 +122,14 @@ class Map {
   getUrl(data) {
     if (!this.links.length) return false;
 
-    const link = `https://www.openstreetmap.org/?mlat=4${data[0].lat}&amp;mlon=${data[0].lon}#map=${this.zoom}/${data[0].lat}/${data[0].lon}`
+    const link = `https://www.openstreetmap.org/?mlat=${data[0].lat}&amp;mlon=${data[0].lon}#map=${this.zoom}/${data[0].lat}/${data[0].lon}`
     this.linkArr.push(link);
     this.setUrl(this.linkArr);
   }
 
   setUrl(arr) {
     this.links.forEach((link, i) => {
-      const valueIndex = Math.floor(i / 2);
-      link.setAttribute(this.attr.href, arr[valueIndex]);
-      console.log(link);
-      console.log(i);
-
-
+      link.setAttribute(this.attr.href, arr[i]);
     })
   }
 }
